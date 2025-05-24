166bc782d4d63f321b063afc0a1e4cf13403376e
tdhancock <tdhancock10@gmail.com>
2025-05-24 13:27:36 -0600

    feat: add tab navigation and icons, implement movie interfaces and image types

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,7 +7,16 @@ const _layout = () => {
     <Tabs>
       <Tabs.Screen
       name="index" 
-      options={{headerShown: false,}} />
+      options={{headerShown: false, title: 'Home'}} />
+      <Tabs.Screen
+        name="search"
+        options={{headerShown: false, title: 'Search'}}/>
+      <Tabs.Screen
+        name="saved"
+        options={{headerShown: false, title: 'Saved'}}/>
+      <Tabs.Screen
+        name="profile"
+        options={{headerShown: false, title: 'Profile'}}/>
     </Tabs>
   )
 }
