a1392fb9fb25c4a9b909ede0f0a498d297a10c94
tdhancock <tdhancock10@gmail.com>
2025-05-26 20:35:40 -0600

    feat: update SearchBar component to use onPress prop for TextInput

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -12,7 +12,7 @@ const SearchBar = ({placeholder, onPress}: Props) => {
   return (
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
       <Image source={icons.search} className="size-5" resizeMode='contain' tintColor="#AB8BFF"></Image>
-      <TextInput onPress={()=>{}} placeholder='Search' value='' onChangeText={()=>{}} placeholderTextColor="#A8B5DB" className='flex-1 ml-2 text-white'></TextInput>
+      <TextInput onPress={onPress} placeholder={placeholder} value='' onChangeText={()=>{}} placeholderTextColor="#A8B5DB" className='flex-1 ml-2 text-white'></TextInput>
     </View>
   )
 }
