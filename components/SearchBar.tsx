c8bce8c766328644acce6bd09c01618669f9a78a
tdhancock <tdhancock10@gmail.com>
2025-05-26 20:30:13 -0600

    feat: implement SearchBar component with styled layout and input functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,12 @@
+import { icons } from '@/constants/icons'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, TextInput, View } from 'react-native'
 
 const SearchBar = () => {
   return (
-    <View>
-      <Text>SearchBar</Text>
+    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+      <Image source={icons.search} className="size-5" resizeMode='contain' tintColor="#AB8BFF"></Image>
+      <TextInput onPress={()=>{}} placeholder='Search' value='' onChangeText={()=>{}} placeholderTextColor="#A8B5DB" className='flex-1 ml-2 text-white'></TextInput>
     </View>
   )
 }
