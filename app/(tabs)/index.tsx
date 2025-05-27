e67b9d5bf06cfe0d3f693154d6e4a7a06faa0954
tdhancock <tdhancock10@gmail.com>
2025-05-26 20:34:06 -0600

    feat: enhance SearchBar component with onPress and placeholder props

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
+
+  const router = useRouter();
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0"/>
@@ -11,7 +15,9 @@ export default function Index() {
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 10, minHeight: '100%' }}>
         <Image source={icons.logo} className="w-22 h-10 mt-20 mb-5 mx-auto" />
         <View className="flex-1 mt-5">
-          <SearchBar />
+          <SearchBar 
+          onPress={() => router.push("/search")}
+          placeholder="Search for a Movie"/>
         </View>
       </ScrollView>
     </View>
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,14 @@
-import { icons } from '@/constants/icons'
-import React from 'react'
-import { Image, TextInput, View } from 'react-native'
+import { icons } from '@/constants/icons';
+import React from 'react';
+import { Image, TextInput, View } from 'react-native';
 
-const SearchBar = () => {
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+}
+
+const SearchBar = ({placeholder, onPress}: Props) => {
+
   return (
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
       <Image source={icons.search} className="size-5" resizeMode='contain' tintColor="#AB8BFF"></Image>
