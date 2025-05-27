99f201f0c0506155b4bec6dfad36e724bc8d1278
tdhancock <tdhancock10@gmail.com>
2025-05-26 20:23:18 -0600

    feat: add SearchBar component and integrate it into the index layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,18 @@
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Image, ScrollView, View } from "react-native";
-
 
 export default function Index() {
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0"/>
 
-      <ScrollView className="flex-1 px-5">
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 10, minHeight: '100%' }}>
         <Image source={icons.logo} className="w-22 h-10 mt-20 mb-5 mx-auto" />
+        <View className="flex-1 mt-5">
+          <SearchBar />
+        </View>
       </ScrollView>
     </View>
   );
