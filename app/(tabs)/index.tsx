b736484954077f4b1d53a98de1c9fc950cc72762
tdhancock <tdhancock10@gmail.com>
2025-05-26 20:18:37 -0600

    feat: restructure index component layout and replace welcome text with background image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
+
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-accent">Welcome! Fuck you.</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0"/>
+
+      <ScrollView className="flex-1 px-5">
+        <Image source={icons.logo} className="w-22 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
     </View>
   );
 }
